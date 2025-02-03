85908420b2811173903745e17a5a963513c9bae9
sh2246 <sh2246@cornell.edu>
2025-02-03 15:39:40 -0500

    update 20250203

--- notebook/20_HyPhyPipeline.sh
+++ notebook/20_HyPhyPipeline.sh
@@ -9,11 +9,11 @@ find output/CDSMSAPerOG_gs/ -name "*.l0.fa" -type f| cut -d / -f 3|sed 's/.gs.l0
 
 ## step 2: get target tip names (based on data/annual_assemblies_20250203.txt)
 mkdir output/target_PAML_20250203
-find output/CDSMSAPerOG_HyPhy_20250203/ -name "*.fa" -type f| cut -d / -f 3|sed 's/.fa//g' |parallel -j 20 "grep -f data/annual_assemblies.txt output/CDSMSAPerOG_HyPhy_20250203/{}.fa|sed 's/>//g' > output/target_PAML_20250203/{}.target"
+find output/CDSMSAPerOG_HyPhy_20250203/ -name "*.fa" -type f| cut -d / -f 3|sed 's/.fa//g' |parallel -j 40 "grep -f data/annual_assemblies_20250203.txt output/CDSMSAPerOG_HyPhy_20250203/{}.fa|sed 's/>//g' > output/target_PAML_20250203/{}.target"
 
 ## step 3: RAxML gene tree generation
 mkdir cd output/geneTree_allOGs_20250203/
-find /workdir/sh2246/p_phyloGWAS/output/CDSMSAPerOG_HyPhy_20250203/ -name '*.fa' -type f| cut -d / -f 7|sed 's/.fa//g' | parallel -j 35 '/workdir/sh2246/p_panAndOGASR/paml_pipeline/standard-RAxML/raxmlHPC -m GTRGAMMA -p 12345 -s /workdir/sh2246/p_phyloGWAS/output/CDSMSAPerOG_HyPhy_20250203/{}.fa -# 1 -w /workdir/sh2246/p_phyloGWAS/output/geneTree_allOGs_20250203/ -n {}.tree'
+find /workdir/sh2246/p_phyloGWAS/output/CDSMSAPerOG_HyPhy_20250203/ -name '*.fa' -type f| cut -d / -f 7|sed 's/.fa//g' | parallel -j 35 'src/standard-RAxML/raxmlHPC -m GTRGAMMA -p 12345 -s /workdir/sh2246/p_phyloGWAS/output/CDSMSAPerOG_HyPhy_20250203/{}.fa -# 1 -w /workdir/sh2246/p_phyloGWAS/output/geneTree_allOGs_20250203/ -n {}.tree'
 
 ## step 4: tree labeling
 find output/geneTree_allOGs/ -name "*.fa" -type f| cut -d / -f 3|sed 's/.fa//g' | parallel -j 10 "Rscript src/LabelNodes_SKH_v7_HyPhyRelax.R output/geneTree_allOGs_20250203/RAxML_bestTree.{}.tree ASM1935983v1 output/target_PAML_20250203/{}.target output/CDSMSAPerOG_HyPhy_20250203/{}.fa output/geneTree_allOGs_20250203/RAxML_Labeled_bestTree.{}_Relax.tree"
