5db34d645549d2550a1c94a081b9eacc3bbe39b8
sh2246 <sh2246@cornell.edu>
2025-04-16 09:38:05 -0400

    20250416 update src/01B

---
+++ src/01B_extractDistToB73.R
@@ -0,0 +1,53 @@
+# Rscript to calculate distance matrix based on MSA
+# author: Sheng-Kai Hsu
+# date created: 2023.09.11
+# date last edited: 2023.09.11
+
+
+rm(list=ls())
+library(ape)
+library(limma)
+library(parallel)
+
+args <- commandArgs(TRUE)
+
+if (args[1] == "--help") {
+  message("################################ HELP ################################\n
+          ## Written by Sheng-Kai Hsu\n## Arguments :\n
+          # --input (required)\n\n
+          # --reference (required) \n\n
+          #--output path to output, default is current directory (required)\n
+          \n######################################################################")
+  q("no")
+}
+
+outDir = getwd()
+i = -1
+while (i < I(length(args)-1)) {
+  i = i + 2
+  if (args[i] %in% c("--input")) {
+    inDir = args[i + 1]
+  }else if (args[i] %in% c("--output")) {
+    outDir = args[i + 1]
+  }
+  else{
+    message("\n### ERROR ###\n\n Argument not recognized:")
+    print(args[i])
+    message("\n### ERROR ###\n")
+    q("no")
+  }
+}
+
+# dat=read.dna(inDir,format = "fasta")
+# dat = dat[sort(rownames(dat)),]
+sampleID = gsub(".txt","",strsplit2(inDir,"/")[,ncol(strsplit2(inDir,"/"))])
+dat = read.table(inDir,header = T)
+refIdx = grepl("Zm-B73",colnames(dat))
+if (refIdx){
+  d  = dat[,refIdx]
+  d = cbind(d, limma::strsplit2(rownames(d),":"))
+  d = d[d[,4]=="0",]
+  d = d[,-4]
+  write.table(d,paste0(outDir,sampleID,"_toB73.txt"),quote = F,sep = "\t")
+  
+}
