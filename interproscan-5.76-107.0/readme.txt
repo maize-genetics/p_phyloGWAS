89d2001e7447373e5e38a6d8be558a3f772e5384
Sheng-Kai Hsu <sh2246@cbsumm26.biohpc.cornell.edu>
2026-02-03 13:24:25 -0500

    20260203 update

---
+++ interproscan-5.76-107.0/readme.txt
@@ -0,0 +1,3 @@
+For more information on downloading, installing, running and configuring InterProScan 5 please see:
+
+https://interproscan-docs.readthedocs.io
--- src/12_runPermulation_perOGModel.R
+++ src/12_runPermulation_perOGModel.R
@@ -96,11 +96,95 @@ fit_model_v4 <- function(OG_id, data, Kmat,traitVec) {
   if(model_full$converge) return(out)
   else return(NULL)
 }
+fit_model_v4_GLM <- function(OG_id, data, Kmat,traitVec) {
+  #   library(asreml)
+  # gc()
+  asreml.options(verbose = FALSE)
+  # print(OG_id)
+  filtered_data <- subset(data, OG == OG_id)
+  filtered_data <- filtered_data[,c("assemblyID","PAV","scaled.dn.ds","scaledESM", "scaledPlantCAD", "PMS",
+                                    "compdup", "queryCov","pacbio")]
+  filtered_data = merge(filtered_data,traitVec,by = "assemblyID")
+  tmp = filtered_data
+  
+  if (length(unique(filtered_data$PAV))==2){
+    fullFM = as.formula(paste0(colnames(traitVec)[2], "~ PAV + PAV:scaled.dn.ds + PAV:scaledESM + PAV:scaledPlantCAD + PAV:PMS + compdup + PAV:queryCov + pacbio"))
+    reducedFM = as.formula(paste0(colnames(traitVec)[2], "~ compdup + queryCov + pacbio"))
+    
+    model_full <- asreml(fixed = fullFM, ai.sing = F, data = filtered_data)
+    # model_reduced1 <- asreml(fixed = fullFM,
+    #                          ai.sing = F, data = filtered_data)
+    filtered_data <- tmp
+    model_reduced <- asreml(fixed = reducedFM, ai.sing = F, data = filtered_data)  
+    
+    totalVar = var(filtered_data[,ncol(filtered_data)])
+    modelSummary <- summary(model_full)
+    IC_full = infoCriteria(model_full,IClikelihood = "full")
+    IC_reduced = infoCriteria(model_reduced,IClikelihood = "full")
+    logLik_full <- IC_full$loglik
+    logLik_reduced <- IC_reduced$loglik
+    BIC_full <- IC_full$BIC
+    BIC_reduced <- IC_reduced$BIC
+    
+    LRT_statistic <- -2 * (logLik_reduced - logLik_full)
+    p_value <- pchisq(LRT_statistic, df = model_reduced$nedf-model_full$nedf, lower.tail = FALSE)
+    # gene_explained <-  (model_reduced2$sigma2-model_full$sigma2)/totalVar
+    phylo_explained <- modelSummary$varcomp$component[1]/sum(modelSummary$varcomp$component)
+    
+    modelWald <- wald.asreml(model_full)
+    
+    partialCoeff <- as.data.frame(model_full$coefficients$fixed)[paste0("PAV_1",c("",":scaled.dn.ds",":scaledESM",":scaledPlantCAD",":PMS_1")),1]
+    partialPval <- as.data.frame(modelWald)[paste0("PAV",c("",":scaled.dn.ds",":scaledESM",":scaledPlantCAD",":PMS")),4]
+    out = c(OG_id,BIC_full,BIC_reduced,LRT_statistic,p_value,phylo_explained,totalVar,partialCoeff,partialPval)
+    names(out) = c("OG","BIC_full","BIC_reduced","LR","p","phyloVAE","totalVar",
+                   paste("partialCoeff",c("PAV","dNdS","ESM2","plantCad","PMS"),sep = "_"),
+                   paste("partialP",c("PAV","dNdS","ESM2","plantCad","PMS"),sep = "_"))
+    
+  } else {
+    fullFM = as.formula(paste0(colnames(traitVec)[2], "~ scaled.dn.ds + scaledESM + scaledPlantCAD + PMS + compdup + queryCov + pacbio"))
+    reducedFM = as.formula(paste0(colnames(traitVec)[2], "~ compdup + queryCov + pacbio"))
+    
+    model_full <- asreml(fixed = fullFM, ai.sing = F, data = filtered_data)
+    # model_reduced1 <- asreml(fixed = fullFM,
+    #                          ai.sing = F, data = filtered_data)
+    filtered_data <- tmp
+    model_reduced <- asreml(fixed = reducedFM, ai.sing = F, data = filtered_data)  
+    
+    totalVar = var(filtered_data[,ncol(filtered_data)])
+    modelSummary <- summary(model_full)
+    IC_full = infoCriteria(model_full,IClikelihood = "full")
+    IC_reduced = infoCriteria(model_reduced,IClikelihood = "full")
+    logLik_full <- IC_full$loglik
+    logLik_reduced <- IC_reduced$loglik
+    BIC_full <- IC_full$BIC
+    BIC_reduced <- IC_reduced$BIC
+    
+    LRT_statistic <- -2 * (logLik_reduced - logLik_full)
+    p_value <- pchisq(LRT_statistic, df = model_reduced$nedf-model_full$nedf, lower.tail = FALSE)
+    # gene_explained <-  (model_reduced2$sigma2-model_full$sigma2)/totalVar
+    phylo_explained <- modelSummary$varcomp$component[1]/sum(modelSummary$varcomp$component)
+    
+    modelWald <- wald.asreml(model_full)
+    partialCoeff <- as.data.frame(model_full$coefficients$fixed)[c("PAV","scaled.dn.ds","scaledESM","scaledPlantCAD","PMS_1"),1]
+    partialPval <- as.data.frame(modelWald)[c("PAV","scaled.dn.ds","scaledESM","scaledPlantCAD","PMS"),4]
+    out = c(OG_id,BIC_full,BIC_reduced,LRT_statistic,p_value,phylo_explained,totalVar,
+            partialCoeff,partialPval)
+  }
+  names(out) = c("OG","BIC_full","BIC_reduced","LR","p","phyloVAE","totalVar",
+                 paste("partialCoeff",c("PAV","dNdS","ESM2","plantCad","PMS"),sep = "_"),
+                 paste("partialP",c("PAV","dNdS","ESM2","plantCad","PMS"),sep = "_"))
+  
+  
+  #     return(model_full)
+  if(model_full$converge) return(out)
+  else return(NULL)
+}
 
 args = commandArgs(trailingOnly=TRUE)
 # arg 1: path to trait table; arg 2: path to master table; arg 3: path to phyloK; arg 4: path to tree;
 # arg 5: name of response variable (e.g. envPC_1);
 # arg 6: output directory; arg 7: permulation or not (TRUE or FALSE)
+# arg 8: GLM or not (TRUE or FALSE)
 
 # dir.trait = "/workdir/sh2246/p_phyloGWAS/output/envData_707Poaceae_20250804.txt"
 # dir.masterTab = "/workdir/sh2246/p_phyloGWAS/output/masterDataTable_PAVFill_20250901_test.txt"
@@ -117,11 +201,13 @@ dir.tree = args[4]
 responseVar = args[5]
 dir.out = args[6]
 permulation = as.logical(args[7])
+GLM = as.logical(args[8])
 
 if(!dir.exists(dir.out)) dir.create(dir.out)
 dir.out = paste0(dir.out,"/",responseVar)
 if(!dir.exists(dir.out)) dir.create(dir.out)
 outDirRes = paste0(dir.out,"/","ASREML_res.txt")
+outDirResGLM = paste0(dir.out,"/","ASREML_GLMres.txt")
 outDirPerm = paste0(dir.out,"/","permulation")
 
 print("start loading data")
@@ -170,6 +256,8 @@ asreml.options(maxit = 40,verbose = F,trace=F)
 test = try(fit_model_v4("OG0000327",data = testData_merged,Kmat = phyloKMat,traitVec = trait_test),silent = T)
 test = try(fit_model_v4("OG0000327",data = testData_merged,Kmat = phyloKMat,traitVec = trait_test),silent = T)
 print(test)
+test = try(fit_model_v4_GLM("OG0000327",data = testData_merged,Kmat = phyloKMat,traitVec = trait_test),silent = T)
+print(test)
 
 if(!file.exists(outDirRes)){
   print("perOG modeling start")
@@ -183,9 +271,24 @@ if(!file.exists(outDirRes)){
   print("perOG modeling done and saved")
 }
 
-testRes = read.table(outDirRes,header =T)
-topOG_list = unique(testRes$OG[testRes$p<0.001])
-print(topOG_list)
+if(!file.exists(outDirResGLM)&GLM){
+  print("perOG GLM modeling start")
+  asreml.options(maxit = 40,verbose = F)
+  testRes = mclapply(OG_list,function(x) try(fit_model_v4_GLM(x,testData_merged,phyloKMat,trait_test),
+                                             silent = T),mc.cores = 35)
+  testRes = t(simplify2array(testRes[sapply(testRes,length)==length(test)]))
+  testRes = as.data.frame(testRes)
+  # testRes = merge(testRes,mappingFileMerged,by = "OG",all.x = T)
+  write.table(testRes,outDirResGLM,quote = F,sep = "\t",row.names = F)
+  print("perOG GLM modeling done and saved")
+}
+
+if(permulation){
+  testRes = read.table(outDirRes,header =T)
+  topOG_list = unique(testRes$OG[testRes$p<0.001])
+} else stop
+
+# print(topOG_list)
 if(permulation&length(topOG_list)>1){
   if(!dir.exists(outDirPerm)) dir.create(outDirPerm)
   
