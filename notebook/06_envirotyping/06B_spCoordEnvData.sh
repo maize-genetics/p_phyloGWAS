e0d01fdbd8b0b5ebd7b82cfd1e904976e88c51a6
ShengKaiHsu <46195776+ShengKaiHsu@users.noreply.github.com>
2026-06-15 15:40:33 -0400

    Update notebook script to run correct metadata processing

--- notebook/06_envirotyping/06B_spCoordEnvData.sh
+++ notebook/06_envirotyping/06B_spCoordEnvData.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # env. data
-# run 12_metadataProcessing.ipynb
+# run 02A_metadataProcessing.ipynb
 # species name -> coordinate
 mkdir output/metadataFormalOut
 Rscript src/08_pulling_geo_data.R data/spNameMetadata_20240819.txt output/metadataFormalOut
